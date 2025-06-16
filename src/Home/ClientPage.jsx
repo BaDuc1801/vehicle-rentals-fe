fea2222ca4a9bb2189f3abb36e950a9775b57c56
unknown <minhduc180104@gmail.com>
2025-06-16 11:09:18 +0700

    fix responsive

--- src/Home/ClientPage.jsx
+++ src/Home/ClientPage.jsx
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux'
 import userService from '../Services/userService'
 import { setUser } from '../Redux/userStore'
 import HeaderNavBar from '../Components/HeaderNavBar'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const ClientPage = () => {
     const dispatch = useDispatch()
@@ -28,6 +30,16 @@ const ClientPage = () => {
 
     return (
         <div className='text-[#0A4348] flex flex-col min-h-screen'>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
             <HeaderNavBar />
             <div className='flex-1 h-[calc(100vh-286px)] bg-[#f2f4f7]'>
                 <Outlet />
